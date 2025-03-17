6a62dde7a86a5dfbf29e5bba633380455296b612
jbshep <jbshep@gmail.com>
2025-03-17 09:22:26 -0500

    Initial code to merge bogus vax and population data

--- merge-and-filter.py
+++ merge-and-filter.py
@@ -6,6 +6,11 @@ pop_df = pd.read_csv("data/bogus-pop.csv")
 vax_df = pd.read_csv("data/bogus-vax.csv")
 
 # Merge them into one data set.
+all_df = pd.merge(pop_df, vax_df, left_on="Geography", right_on="State")
+del all_df["Geography"]
+print(all_df)
 
 # Use new dataset to calculate population density and display
 # state, vaccination rate, and population density.
+all_df["Density"] = all_df["Population"] / all_df["Area"]
+print(all_df[["State", "Vax Perc", "Density"]])
